0a5c9eba3b1785bff76c704acf1149dca5df9dbb
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-11-21 20:29:36 +0700

    Hàm cập nhật sensor theo chu kỳ

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ prev_temp = 1
 new_temp = 1
 prev_humid = 0
 new_humid = 0
+max_temp = 100
+max_humid = 90
 # khởi tạo màn hình LCD
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)
 lcd = I2cLcd(i2c, 0x27, 2, 16)
 # khởi tạo buzzer
 buzzer = PWM(Pin(4))
+is_buzzer_playing = False
 
 
 # hàm lấy dữ liệu từ DHT22
@@ -31,7 +34,6 @@ def read_sensor():
 
 # hàm hiển thị ra màn hình LCD
 def lcd_output(string):
-    global lcd
     lcd.clear()
     lcd.putstr(string)
 
@@ -50,6 +52,8 @@ def be_quiet():
 
 
 def play_song(song, tempo):
+    global is_buzzer_playing
+    is_buzzer_playing = True
     for note in range(len(song)):
         # nếu nốt nhạc = rest, không phát âm thanh
         if song[note] == "R":
@@ -59,7 +63,25 @@ def play_song(song, tempo):
             play_tone(tones[song[note]])
         # tiếp tục phát âm trong quãng thời gian ứng với độ dài một nốt nhạc
         sleep(60 / tempo)
+    is_buzzer_playing = False
     be_quiet()
+
+
+# Hàm cập nhật các sensor theo chu kỳ
+def update_sensors():
+    # cần bổ sung hàm cập nhật nhiệt độ, độ ẩm ở đây
+
+    if new_temp != prev_temp or new_humid != prev_humid:
+        lcd_output(f"{new_temp}, {new_humid}")
+    # if not is_buzzer_playing:
+    #     if new_temp > max_temp and new_humid > max_humid:
+    #         play_song(song1, tempo1)
+    #     elif new_temp > max_temp:
+    #         play_song(song2, tempo2)
+    #     elif new_humid > max_humid:
+    #         play_song(song3, tempo3)
+
+    # Cần bổ sung hàm cập nhật led ở đây
 
 
 # các hàm xử lý websocket
@@ -92,6 +114,8 @@ def _closed_callback(websocket):
     print("WebSocket closed")
 
 
+# Thực hiện cập nhật sensor theo chu kỳ
+_thread.start_new_thread(update_sensors, ())
 # tạo máy chủ webserver
 mws = MicroWebSrv(webPath="/www")  # TCP port 80 and files in /www
 mws.AcceptWebSocketCallback = _accept_websocket_callback
